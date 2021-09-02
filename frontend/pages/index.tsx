32b022752c06fe094f8cea3320356ef6128a9002
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-09-02 19:42:39 +0200

    Test the material-ui typography component

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,8 @@
 import type { NextPage } from 'next';
+import { Typography } from '@material-ui/core';
 
 const Home: NextPage = () => {
-  return <h1>Hello, world!</h1>;
+  return <Typography variant="h1">Hello, world!</Typography>;
 };
 
 export default Home;
