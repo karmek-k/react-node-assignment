5c59218c6a51be3b3477a32169d0785b052daf56
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-09-02 20:06:20 +0200

    Add some styling to the TodoList component

--- frontend/components/TodoList.tsx
+++ frontend/components/TodoList.tsx
@@ -1,10 +1,26 @@
 import { Paper, Typography } from '@material-ui/core';
 import React from 'react';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles({
+  listContainer: {
+    padding: '1rem',
+    maxWidth: '600px',
+    margin: 'auto'
+  },
+  textCentered: {
+    textAlign: 'center'
+  }
+});
 
 const TodoList: React.FC = () => {
+  const classes = useStyles();
+
   return (
-    <Paper>
-      <Typography variant="h3">To-do List</Typography>
+    <Paper className={classes.listContainer}>
+      <Typography variant="h3" className={classes.textCentered}>
+        To-do List
+      </Typography>
     </Paper>
   );
 };
