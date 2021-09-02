0d0dcbcd811e1032d963a95c783f791fe338c675
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-09-02 20:08:50 +0200

    Move TodoList component to components/index

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import TodoList from '../components/TodoList';
+import TodoList from '../components/index/TodoList';
 
 const Home: NextPage = () => {
   return (
