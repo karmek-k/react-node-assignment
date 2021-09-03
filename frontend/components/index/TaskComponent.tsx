f726b55c85e2b0c96203416acd793c5f9a3b39b9
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-09-03 15:17:06 +0200

    Add toggle done button

--- frontend/components/index/TaskComponent.tsx
+++ frontend/components/index/TaskComponent.tsx
@@ -1,6 +1,7 @@
 import { Button, ListItem, ListItemText } from '@material-ui/core';
 import React from 'react';
 import useDeleteTask from '../../hooks/useDeleteTask';
+import useToggleDone from '../../hooks/useToggleDone';
 import { Task } from '../../interfaces/Task';
 
 interface Props {
@@ -9,10 +10,18 @@ interface Props {
 
 const TaskComponent: React.FC<Props> = ({ task }) => {
   const deleteTask = useDeleteTask();
+  const toggleDone = useToggleDone();
 
   return (
     <ListItem>
       <ListItemText primary={task.name} />
+      <Button
+        variant="outlined"
+        color="secondary"
+        onClick={() => toggleDone(task.id)}
+      >
+        Toggle done
+      </Button>
       <Button
         variant="contained"
         color="secondary"
