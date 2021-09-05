bd24d67c5beac091714a7e293e57c65466d197da
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-09-05 19:18:20 +0200

    Add a theme

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,14 +3,18 @@ import Head from 'next/head';
 import TodoList from '../components/index/TodoList';
 import taskStore from '../redux/store';
 import { Provider as ReduxProvider } from 'react-redux';
+import { ThemeProvider } from '@material-ui/core';
+import orange from '../theme/orange';
 
 const Home: NextPage = () => {
   return (
     <ReduxProvider store={taskStore}>
-      <Head>
-        <title>To-do List</title>
-      </Head>
-      <TodoList />
+      <ThemeProvider theme={orange}>
+        <Head>
+          <title>To-do List</title>
+        </Head>
+        <TodoList />
+      </ThemeProvider>
     </ReduxProvider>
   );
 };
