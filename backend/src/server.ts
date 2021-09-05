290a6b66d416f3e3e338894b8b3c4d145a7cfe50
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-09-05 13:52:01 +0200

    Apply cors middleware

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import TaskRouter from './routes/task';
 
 const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.use('/task', TaskRouter);
