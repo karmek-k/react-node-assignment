d8eba6c3f6caf76c6d2f9d845e6e52ad0cfcf4d9
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-09-06 16:57:19 +0200

    Modify generateTasks and useApiList implementation

--- frontend/hooks/api/useApiList.ts
+++ frontend/hooks/api/useApiList.ts
@@ -2,12 +2,12 @@ import { useEffect, useState } from 'react';
 import { Task } from '../../interfaces/Task';
 import { listTasks } from '../../resources/task';
 
-const useApiList = () => {
+const useApiList = (page: number) => {
   const [tasks, setTasks] = useState<Task[] | null>(null);
 
   useEffect(() => {
-    listTasks().then(setTasks);
-  }, []);
+    listTasks(page).then(setTasks);
+  }, [page]);
 
   return tasks;
 };
--- frontend/resources/task.ts
+++ frontend/resources/task.ts
@@ -8,8 +8,8 @@ import {
 } from '../interfaces/Task';
 import api from '../utils/api';
 
-export const listTasks = async () => {
-  const { data } = await api.get<TaskListResponse>('/task');
+export const listTasks = async (page: number) => {
+  const { data } = await api.get<TaskListResponse>(`/task?page=${page}`);
 
   return data.tasks;
 };
@@ -35,9 +35,7 @@ export const deleteTask = async (id: number) => {
 };
 
 export const generateTasks = async (count: number) => {
-  const { data } = await api.post<TaskGenerateResponse>('/task/generate', {
+  await api.post<TaskGenerateResponse>('/task/generate', {
     count
   });
-
-  return data.tasks;
 };
