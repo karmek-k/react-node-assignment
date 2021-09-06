f73fa7c2b43a722515eb34ddf70dd07e0b6c1099
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-09-06 17:01:31 +0200

    Change the implementation of useApiGenerate

--- frontend/hooks/api/useApiGenerate.ts
+++ frontend/hooks/api/useApiGenerate.ts
@@ -1,13 +1,10 @@
 import { generateTasks } from '../../resources/task';
-import useCreateTask from '../useCreateTask';
 
 const useApiGenerate = () => {
-  const createTaskInStore = useCreateTask();
-
   return (count: number) => {
-    generateTasks(count).then(tasks => {
-      tasks.forEach(createTaskInStore);
-    });
+    generateTasks(count).then(() =>
+      alert('Tasks have been generated. Please refresh the page.')
+    );
   };
 };
 
